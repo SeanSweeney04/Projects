1d33b57e3e1ad75e5a04e2a02694a4bb003f88cb
SeanSweeney04 <115557622+SeanSweeney04@users.noreply.github.com>
2023-05-20 17:08:49 +0100

    Add files via upload
    This is a program that helps you progressively overload with your workout in the gym, showing you the results from your last workout which helps you get an idea for your goals for your current workout, and shows you the results that you can review at the end of your workout.

---
+++ workoutracker.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+
+print("Here are the results from your last workout")
+print("--------------------")
+
+f = open("latest.txt", "r")
+print(f.read())
+
+
+
+class Workout(object):
+    def __init__(self, exercise, weight, reps):
+        self.exercise = exercise
+        self.d = {}
+        self.weight = weight
+        self.reps = reps
+    def __str__(self):
+        r = []
+        r.append(f'Exrecise: {self.exercise}')
+        r.append(f'Weight: {self.weight} KG')
+        r.append(f'Reps: {self.reps}')
+        return '\n'.join(r)
+f = open("latest.txt", "w")
+a = []
+q = input("Add an exercise? (Y/N) ")
+while q != "N":
+   if q == "Y":
+      ex = input("Exercise: ")
+      f.write(ex + '\n')
+      wght = input("Weight: ")
+      f.write(wght + "KG" +'\n')
+      rps = input("Reps: ")
+      f.write(rps + "reps" + '\n' + '\n')
+      a.append(Workout(ex, wght, rps))
+      q = input("Add an exercise? (Y/N) ")
+f.close()
+print("Workout results" + "\n" + "-------------")
+for e in a:
+    print(e)
