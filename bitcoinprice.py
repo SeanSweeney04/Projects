517cdfbcf288add1abcbebc0521fe9fec27d2d2e
SeanSweeney04 <115557622+SeanSweeney04@users.noreply.github.com>
2023-05-30 14:31:38 +0100

    Create bitcoinprice.py
    This is the first API I have ever made which retrieves bitcoin's price.

---
+++ bitcoinprice.py
@@ -0,0 +1,7 @@
+import requests
+
+response = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
+price = response.json()
+bitcoin_price = price['bitcoin']['usd']
+
+print("Bitcoin is priced at $",bitcoin_price, "today.")
