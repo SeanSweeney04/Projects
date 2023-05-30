97cec64553422ec489a89e9583e56fc717c4bf3a
SeanSweeney04 <115557622+SeanSweeney04@users.noreply.github.com>
2023-05-30 14:34:42 +0100

    Update bitcoinprice.py

--- bitcoinprice.py
+++ bitcoinprice.py
@@ -1,5 +1,5 @@
 import requests
-
+# This is the first API I have ever made, which retrieves Bitcoin's current price.
 response = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
 price = response.json()
 bitcoin_price = price['bitcoin']['usd']
